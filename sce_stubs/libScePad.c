334f1c407f719a2ad81a1e341e2e15db82a30eb8
John Törnblom <john.tornblom@gmail.com>
2024-05-08 22:23:00 +0200

    add stub for libScePad

---
+++ sce_stubs/libScePad.c
@@ -0,0 +1,520 @@
+
+asm(".global scePadBondUpdateGetReconnectStatus\n"
+    ".type scePadBondUpdateGetReconnectStatus @function\n"
+    "scePadBondUpdateGetReconnectStatus:\n");
+
+asm(".global scePadBondUpdateGetStatus\n"
+    ".type scePadBondUpdateGetStatus @function\n"
+    "scePadBondUpdateGetStatus:\n");
+
+asm(".global scePadBondUpdateInit\n"
+    ".type scePadBondUpdateInit @function\n"
+    "scePadBondUpdateInit:\n");
+
+asm(".global scePadBondUpdateIsReady\n"
+    ".type scePadBondUpdateIsReady @function\n"
+    "scePadBondUpdateIsReady:\n");
+
+asm(".global scePadBondUpdateResetBond\n"
+    ".type scePadBondUpdateResetBond @function\n"
+    "scePadBondUpdateResetBond:\n");
+
+asm(".global scePadBondUpdateSendData\n"
+    ".type scePadBondUpdateSendData @function\n"
+    "scePadBondUpdateSendData:\n");
+
+asm(".global scePadBondUpdateStart\n"
+    ".type scePadBondUpdateStart @function\n"
+    "scePadBondUpdateStart:\n");
+
+asm(".global scePadBondUpdateTerm\n"
+    ".type scePadBondUpdateTerm @function\n"
+    "scePadBondUpdateTerm:\n");
+
+asm(".global scePadClose\n"
+    ".type scePadClose @function\n"
+    "scePadClose:\n");
+
+asm(".global scePadConnectPort\n"
+    ".type scePadConnectPort @function\n"
+    "scePadConnectPort:\n");
+
+asm(".global scePadDeviceClassGetExtendedInformation\n"
+    ".type scePadDeviceClassGetExtendedInformation @function\n"
+    "scePadDeviceClassGetExtendedInformation:\n");
+
+asm(".global scePadDeviceClassParseData\n"
+    ".type scePadDeviceClassParseData @function\n"
+    "scePadDeviceClassParseData:\n");
+
+asm(".global scePadDeviceOpen\n"
+    ".type scePadDeviceOpen @function\n"
+    "scePadDeviceOpen:\n");
+
+asm(".global scePadDisableTrackingLedBatterySaver\n"
+    ".type scePadDisableTrackingLedBatterySaver @function\n"
+    "scePadDisableTrackingLedBatterySaver:\n");
+
+asm(".global scePadDisableTransportButtons\n"
+    ".type scePadDisableTransportButtons @function\n"
+    "scePadDisableTransportButtons:\n");
+
+asm(".global scePadDisableTriggerEffect\n"
+    ".type scePadDisableTriggerEffect @function\n"
+    "scePadDisableTriggerEffect:\n");
+
+asm(".global scePadDisableVibration\n"
+    ".type scePadDisableVibration @function\n"
+    "scePadDisableVibration:\n");
+
+asm(".global scePadDisconnectDevice\n"
+    ".type scePadDisconnectDevice @function\n"
+    "scePadDisconnectDevice:\n");
+
+asm(".global scePadDisconnectPort\n"
+    ".type scePadDisconnectPort @function\n"
+    "scePadDisconnectPort:\n");
+
+asm(".global scePadEnableAutoDetect\n"
+    ".type scePadEnableAutoDetect @function\n"
+    "scePadEnableAutoDetect:\n");
+
+asm(".global scePadEnableExtensionPort\n"
+    ".type scePadEnableExtensionPort @function\n"
+    "scePadEnableExtensionPort:\n");
+
+asm(".global scePadEnablePlayPauseEmulation\n"
+    ".type scePadEnablePlayPauseEmulation @function\n"
+    "scePadEnablePlayPauseEmulation:\n");
+
+asm(".global scePadEnablePs4CompatibleMode\n"
+    ".type scePadEnablePs4CompatibleMode @function\n"
+    "scePadEnablePs4CompatibleMode:\n");
+
+asm(".global scePadEnableSpecificDeviceClass\n"
+    ".type scePadEnableSpecificDeviceClass @function\n"
+    "scePadEnableSpecificDeviceClass:\n");
+
+asm(".global scePadEnableUsbConnection\n"
+    ".type scePadEnableUsbConnection @function\n"
+    "scePadEnableUsbConnection:\n");
+
+asm(".global scePadGetActiveTime\n"
+    ".type scePadGetActiveTime @function\n"
+    "scePadGetActiveTime:\n");
+
+asm(".global scePadGetBluetoothAddress\n"
+    ".type scePadGetBluetoothAddress @function\n"
+    "scePadGetBluetoothAddress:\n");
+
+asm(".global scePadGetCapability\n"
+    ".type scePadGetCapability @function\n"
+    "scePadGetCapability:\n");
+
+asm(".global scePadGetControllerInformation\n"
+    ".type scePadGetControllerInformation @function\n"
+    "scePadGetControllerInformation:\n");
+
+asm(".global scePadGetDataInternal\n"
+    ".type scePadGetDataInternal @function\n"
+    "scePadGetDataInternal:\n");
+
+asm(".global scePadGetDeviceId\n"
+    ".type scePadGetDeviceId @function\n"
+    "scePadGetDeviceId:\n");
+
+asm(".global scePadGetDeviceInfo\n"
+    ".type scePadGetDeviceInfo @function\n"
+    "scePadGetDeviceInfo:\n");
+
+asm(".global scePadGetExtControllerInformation\n"
+    ".type scePadGetExtControllerInformation @function\n"
+    "scePadGetExtControllerInformation:\n");
+
+asm(".global scePadGetExtensionUnitInfo\n"
+    ".type scePadGetExtensionUnitInfo @function\n"
+    "scePadGetExtensionUnitInfo:\n");
+
+asm(".global scePadGetFeatureReport\n"
+    ".type scePadGetFeatureReport @function\n"
+    "scePadGetFeatureReport:\n");
+
+asm(".global scePadGetFeatureReport2\n"
+    ".type scePadGetFeatureReport2 @function\n"
+    "scePadGetFeatureReport2:\n");
+
+asm(".global scePadGetHandle\n"
+    ".type scePadGetHandle @function\n"
+    "scePadGetHandle:\n");
+
+asm(".global scePadGetIdleCount\n"
+    ".type scePadGetIdleCount @function\n"
+    "scePadGetIdleCount:\n");
+
+asm(".global scePadGetInfo\n"
+    ".type scePadGetInfo @function\n"
+    "scePadGetInfo:\n");
+
+asm(".global scePadGetInfoByPortType\n"
+    ".type scePadGetInfoByPortType @function\n"
+    "scePadGetInfoByPortType:\n");
+
+asm(".global scePadGetLicenseControllerInformation\n"
+    ".type scePadGetLicenseControllerInformation @function\n"
+    "scePadGetLicenseControllerInformation:\n");
+
+asm(".global scePadGetLotusIrCodeSetResult\n"
+    ".type scePadGetLotusIrCodeSetResult @function\n"
+    "scePadGetLotusIrCodeSetResult:\n");
+
+asm(".global scePadGetMotionSensorPosition\n"
+    ".type scePadGetMotionSensorPosition @function\n"
+    "scePadGetMotionSensorPosition:\n");
+
+asm(".global scePadGetMotionTimerUnit\n"
+    ".type scePadGetMotionTimerUnit @function\n"
+    "scePadGetMotionTimerUnit:\n");
+
+asm(".global scePadGetSphereRadius\n"
+    ".type scePadGetSphereRadius @function\n"
+    "scePadGetSphereRadius:\n");
+
+asm(".global scePadGetTriggerEffectInternalState\n"
+    ".type scePadGetTriggerEffectInternalState @function\n"
+    "scePadGetTriggerEffectInternalState:\n");
+
+asm(".global scePadGetTriggerEffectState\n"
+    ".type scePadGetTriggerEffectState @function\n"
+    "scePadGetTriggerEffectState:\n");
+
+asm(".global scePadGetVersionInfo\n"
+    ".type scePadGetVersionInfo @function\n"
+    "scePadGetVersionInfo:\n");
+
+asm(".global scePadInit\n"
+    ".type scePadInit @function\n"
+    "scePadInit:\n");
+
+asm(".global scePadIsBlasterConnected\n"
+    ".type scePadIsBlasterConnected @function\n"
+    "scePadIsBlasterConnected:\n");
+
+asm(".global scePadIsDS4Connected\n"
+    ".type scePadIsDS4Connected @function\n"
+    "scePadIsDS4Connected:\n");
+
+asm(".global scePadIsLightBarBaseBrightnessControllable\n"
+    ".type scePadIsLightBarBaseBrightnessControllable @function\n"
+    "scePadIsLightBarBaseBrightnessControllable:\n");
+
+asm(".global scePadIsMoveConnected\n"
+    ".type scePadIsMoveConnected @function\n"
+    "scePadIsMoveConnected:\n");
+
+asm(".global scePadIsMoveReproductionModel\n"
+    ".type scePadIsMoveReproductionModel @function\n"
+    "scePadIsMoveReproductionModel:\n");
+
+asm(".global scePadIsValidHandle\n"
+    ".type scePadIsValidHandle @function\n"
+    "scePadIsValidHandle:\n");
+
+asm(".global scePadLastLotusButtonData\n"
+    ".type scePadLastLotusButtonData @function\n"
+    "scePadLastLotusButtonData:\n");
+
+asm(".global scePadMbusInit\n"
+    ".type scePadMbusInit @function\n"
+    "scePadMbusInit:\n");
+
+asm(".global scePadMbusTerm\n"
+    ".type scePadMbusTerm @function\n"
+    "scePadMbusTerm:\n");
+
+asm(".global scePadOpen\n"
+    ".type scePadOpen @function\n"
+    "scePadOpen:\n");
+
+asm(".global scePadOpenExt\n"
+    ".type scePadOpenExt @function\n"
+    "scePadOpenExt:\n");
+
+asm(".global scePadOpenExt2\n"
+    ".type scePadOpenExt2 @function\n"
+    "scePadOpenExt2:\n");
+
+asm(".global scePadOutputReport\n"
+    ".type scePadOutputReport @function\n"
+    "scePadOutputReport:\n");
+
+asm(".global scePadRead\n"
+    ".type scePadRead @function\n"
+    "scePadRead:\n");
+
+asm(".global scePadReadBlasterForTracker\n"
+    ".type scePadReadBlasterForTracker @function\n"
+    "scePadReadBlasterForTracker:\n");
+
+asm(".global scePadReadExt\n"
+    ".type scePadReadExt @function\n"
+    "scePadReadExt:\n");
+
+asm(".global scePadReadForTracker\n"
+    ".type scePadReadForTracker @function\n"
+    "scePadReadForTracker:\n");
+
+asm(".global scePadReadHistory\n"
+    ".type scePadReadHistory @function\n"
+    "scePadReadHistory:\n");
+
+asm(".global scePadReadState\n"
+    ".type scePadReadState @function\n"
+    "scePadReadState:\n");
+
+asm(".global scePadReadStateExt\n"
+    ".type scePadReadStateExt @function\n"
+    "scePadReadStateExt:\n");
+
+asm(".global scePadRemoteControlInsertData\n"
+    ".type scePadRemoteControlInsertData @function\n"
+    "scePadRemoteControlInsertData:\n");
+
+asm(".global scePadResetLightBar\n"
+    ".type scePadResetLightBar @function\n"
+    "scePadResetLightBar:\n");
+
+asm(".global scePadResetLightBarAll\n"
+    ".type scePadResetLightBarAll @function\n"
+    "scePadResetLightBarAll:\n");
+
+asm(".global scePadResetLightBarAllByPortType\n"
+    ".type scePadResetLightBarAllByPortType @function\n"
+    "scePadResetLightBarAllByPortType:\n");
+
+asm(".global scePadResetOrientation\n"
+    ".type scePadResetOrientation @function\n"
+    "scePadResetOrientation:\n");
+
+asm(".global scePadResetOrientationForTracker\n"
+    ".type scePadResetOrientationForTracker @function\n"
+    "scePadResetOrientationForTracker:\n");
+
+asm(".global scePadSetAngularVelocityBiasCorrectionState\n"
+    ".type scePadSetAngularVelocityBiasCorrectionState @function\n"
+    "scePadSetAngularVelocityBiasCorrectionState:\n");
+
+asm(".global scePadSetAngularVelocityDeadbandState\n"
+    ".type scePadSetAngularVelocityDeadbandState @function\n"
+    "scePadSetAngularVelocityDeadbandState:\n");
+
+asm(".global scePadSetAutoPowerOffCount\n"
+    ".type scePadSetAutoPowerOffCount @function\n"
+    "scePadSetAutoPowerOffCount:\n");
+
+asm(".global scePadSetButtonRemappingInfo\n"
+    ".type scePadSetButtonRemappingInfo @function\n"
+    "scePadSetButtonRemappingInfo:\n");
+
+asm(".global scePadSetExtensionReport\n"
+    ".type scePadSetExtensionReport @function\n"
+    "scePadSetExtensionReport:\n");
+
+asm(".global scePadSetFeatureReport\n"
+    ".type scePadSetFeatureReport @function\n"
+    "scePadSetFeatureReport:\n");
+
+asm(".global scePadSetFeatureReport2\n"
+    ".type scePadSetFeatureReport2 @function\n"
+    "scePadSetFeatureReport2:\n");
+
+asm(".global scePadSetForceIntercepted\n"
+    ".type scePadSetForceIntercepted @function\n"
+    "scePadSetForceIntercepted:\n");
+
+asm(".global scePadSetLightBar\n"
+    ".type scePadSetLightBar @function\n"
+    "scePadSetLightBar:\n");
+
+asm(".global scePadSetLightBarBaseBrightness\n"
+    ".type scePadSetLightBarBaseBrightness @function\n"
+    "scePadSetLightBarBaseBrightness:\n");
+
+asm(".global scePadSetLightBarBlinking\n"
+    ".type scePadSetLightBarBlinking @function\n"
+    "scePadSetLightBarBlinking:\n");
+
+asm(".global scePadSetLightBarForTracker\n"
+    ".type scePadSetLightBarForTracker @function\n"
+    "scePadSetLightBarForTracker:\n");
+
+asm(".global scePadSetLoginUserNumber\n"
+    ".type scePadSetLoginUserNumber @function\n"
+    "scePadSetLoginUserNumber:\n");
+
+asm(".global scePadSetLotusDisableIrKey\n"
+    ".type scePadSetLotusDisableIrKey @function\n"
+    "scePadSetLotusDisableIrKey:\n");
+
+asm(".global scePadSetLotusEnableIrKey\n"
+    ".type scePadSetLotusEnableIrKey @function\n"
+    "scePadSetLotusEnableIrKey:\n");
+
+asm(".global scePadSetLotusIrCodeSetData\n"
+    ".type scePadSetLotusIrCodeSetData @function\n"
+    "scePadSetLotusIrCodeSetData:\n");
+
+asm(".global scePadSetLotusIrCodeSetEnd\n"
+    ".type scePadSetLotusIrCodeSetEnd @function\n"
+    "scePadSetLotusIrCodeSetEnd:\n");
+
+asm(".global scePadSetLotusIrCodeSetStart\n"
+    ".type scePadSetLotusIrCodeSetStart @function\n"
+    "scePadSetLotusIrCodeSetStart:\n");
+
+asm(".global scePadSetMotionSensorState\n"
+    ".type scePadSetMotionSensorState @function\n"
+    "scePadSetMotionSensorState:\n");
+
+asm(".global scePadSetPS4BcVibrationMode\n"
+    ".type scePadSetPS4BcVibrationMode @function\n"
+    "scePadSetPS4BcVibrationMode:\n");
+
+asm(".global scePadSetProcessFocus\n"
+    ".type scePadSetProcessFocus @function\n"
+    "scePadSetProcessFocus:\n");
+
+asm(".global scePadSetProcessPrivilege\n"
+    ".type scePadSetProcessPrivilege @function\n"
+    "scePadSetProcessPrivilege:\n");
+
+asm(".global scePadSetProcessPrivilegeOfButtonRemapping\n"
+    ".type scePadSetProcessPrivilegeOfButtonRemapping @function\n"
+    "scePadSetProcessPrivilegeOfButtonRemapping:\n");
+
+asm(".global scePadSetShareButtonMaskForRemotePlay\n"
+    ".type scePadSetShareButtonMaskForRemotePlay @function\n"
+    "scePadSetShareButtonMaskForRemotePlay:\n");
+
+asm(".global scePadSetTiltCorrectionState\n"
+    ".type scePadSetTiltCorrectionState @function\n"
+    "scePadSetTiltCorrectionState:\n");
+
+asm(".global scePadSetTriggerEffect\n"
+    ".type scePadSetTriggerEffect @function\n"
+    "scePadSetTriggerEffect:\n");
+
+asm(".global scePadSetTriggerEffectStrength\n"
+    ".type scePadSetTriggerEffectStrength @function\n"
+    "scePadSetTriggerEffectStrength:\n");
+
+asm(".global scePadSetUserColor\n"
+    ".type scePadSetUserColor @function\n"
+    "scePadSetUserColor:\n");
+
+asm(".global scePadSetUserNumber\n"
+    ".type scePadSetUserNumber @function\n"
+    "scePadSetUserNumber:\n");
+
+asm(".global scePadSetVibration\n"
+    ".type scePadSetVibration @function\n"
+    "scePadSetVibration:\n");
+
+asm(".global scePadSetVibrationForce\n"
+    ".type scePadSetVibrationForce @function\n"
+    "scePadSetVibrationForce:\n");
+
+asm(".global scePadSetVibrationMode\n"
+    ".type scePadSetVibrationMode @function\n"
+    "scePadSetVibrationMode:\n");
+
+asm(".global scePadSetVibrationStrength\n"
+    ".type scePadSetVibrationStrength @function\n"
+    "scePadSetVibrationStrength:\n");
+
+asm(".global scePadSetVibrationTriggerEffectWeakWhileEmbeddedMicInUse\n"
+    ".type scePadSetVibrationTriggerEffectWeakWhileEmbeddedMicInUse @function\n"
+    "scePadSetVibrationTriggerEffectWeakWhileEmbeddedMicInUse:\n");
+
+asm(".global scePadSetVrTrackingMode\n"
+    ".type scePadSetVrTrackingMode @function\n"
+    "scePadSetVrTrackingMode:\n");
+
+asm(".global scePadShareOutputData\n"
+    ".type scePadShareOutputData @function\n"
+    "scePadShareOutputData:\n");
+
+asm(".global scePadShareOutputDataWithFunctionType\n"
+    ".type scePadShareOutputDataWithFunctionType @function\n"
+    "scePadShareOutputDataWithFunctionType:\n");
+
+asm(".global scePadStartRecording\n"
+    ".type scePadStartRecording @function\n"
+    "scePadStartRecording:\n");
+
+asm(".global scePadStopRecording\n"
+    ".type scePadStopRecording @function\n"
+    "scePadStopRecording:\n");
+
+asm(".global scePadSwitchConnection\n"
+    ".type scePadSwitchConnection @function\n"
+    "scePadSwitchConnection:\n");
+
+asm(".global scePadVertualDeviceAddDevice\n"
+    ".type scePadVertualDeviceAddDevice @function\n"
+    "scePadVertualDeviceAddDevice:\n");
+
+asm(".global scePadVirtualDeviceAddDevice\n"
+    ".type scePadVirtualDeviceAddDevice @function\n"
+    "scePadVirtualDeviceAddDevice:\n");
+
+asm(".global scePadVirtualDeviceDeleteDevice\n"
+    ".type scePadVirtualDeviceDeleteDevice @function\n"
+    "scePadVirtualDeviceDeleteDevice:\n");
+
+asm(".global scePadVirtualDeviceDisableButtonRemapping\n"
+    ".type scePadVirtualDeviceDisableButtonRemapping @function\n"
+    "scePadVirtualDeviceDisableButtonRemapping:\n");
+
+asm(".global scePadVirtualDeviceGetRemoteSetting\n"
+    ".type scePadVirtualDeviceGetRemoteSetting @function\n"
+    "scePadVirtualDeviceGetRemoteSetting:\n");
+
+asm(".global scePadVirtualDeviceInsertData\n"
+    ".type scePadVirtualDeviceInsertData @function\n"
+    "scePadVirtualDeviceInsertData:\n");
+
+asm(".global scePadVirtualDeviceInsertTriggerInternalState\n"
+    ".type scePadVirtualDeviceInsertTriggerInternalState @function\n"
+    "scePadVirtualDeviceInsertTriggerInternalState:\n");
+
+asm(".global scePadVirtualDeviceSetTriggerEffectSetting\n"
+    ".type scePadVirtualDeviceSetTriggerEffectSetting @function\n"
+    "scePadVirtualDeviceSetTriggerEffectSetting:\n");
+
+asm(".global scePadVrControllerGetDeviceInformation\n"
+    ".type scePadVrControllerGetDeviceInformation @function\n"
+    "scePadVrControllerGetDeviceInformation:\n");
+
+asm(".global scePadVrControllerGetTriggerEffectState\n"
+    ".type scePadVrControllerGetTriggerEffectState @function\n"
+    "scePadVrControllerGetTriggerEffectState:\n");
+
+asm(".global scePadVrControllerRead\n"
+    ".type scePadVrControllerRead @function\n"
+    "scePadVrControllerRead:\n");
+
+asm(".global scePadVrControllerReadCombinedState\n"
+    ".type scePadVrControllerReadCombinedState @function\n"
+    "scePadVrControllerReadCombinedState:\n");
+
+asm(".global scePadVrControllerReadState\n"
+    ".type scePadVrControllerReadState @function\n"
+    "scePadVrControllerReadState:\n");
+
+asm(".global scePadVrControllerSetTriggerEffect\n"
+    ".type scePadVrControllerSetTriggerEffect @function\n"
+    "scePadVrControllerSetTriggerEffect:\n");
+
+asm(".global scePadVrControllerSetTriggerEffects\n"
+    ".type scePadVrControllerSetTriggerEffects @function\n"
+    "scePadVrControllerSetTriggerEffects:\n");
