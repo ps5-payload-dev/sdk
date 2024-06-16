d4a33b7cba9b80e57ba3525cec712e813924ff1b
John Törnblom <john.tornblom@gmail.com>
2024-06-16 18:16:57 +0200

    add stubs for libSceKeyboard

---
+++ sce_stubs/libSceKeyboard.c
@@ -0,0 +1,88 @@
+
+asm(".global sceDbgKeyboardClose\n"
+    ".type sceDbgKeyboardClose @function\n"
+    "sceDbgKeyboardClose:\n");
+
+asm(".global sceDbgKeyboardGetKey2Char\n"
+    ".type sceDbgKeyboardGetKey2Char @function\n"
+    "sceDbgKeyboardGetKey2Char:\n");
+
+asm(".global sceDbgKeyboardInit\n"
+    ".type sceDbgKeyboardInit @function\n"
+    "sceDbgKeyboardInit:\n");
+
+asm(".global sceDbgKeyboardOpen\n"
+    ".type sceDbgKeyboardOpen @function\n"
+    "sceDbgKeyboardOpen:\n");
+
+asm(".global sceDbgKeyboardRead\n"
+    ".type sceDbgKeyboardRead @function\n"
+    "sceDbgKeyboardRead:\n");
+
+asm(".global sceDbgKeyboardReadState\n"
+    ".type sceDbgKeyboardReadState @function\n"
+    "sceDbgKeyboardReadState:\n");
+
+asm(".global sceKeyboardClose\n"
+    ".type sceKeyboardClose @function\n"
+    "sceKeyboardClose:\n");
+
+asm(".global sceKeyboardConnectPort\n"
+    ".type sceKeyboardConnectPort @function\n"
+    "sceKeyboardConnectPort:\n");
+
+asm(".global sceKeyboardDebugGetDeviceId\n"
+    ".type sceKeyboardDebugGetDeviceId @function\n"
+    "sceKeyboardDebugGetDeviceId:\n");
+
+asm(".global sceKeyboardDeviceOpen\n"
+    ".type sceKeyboardDeviceOpen @function\n"
+    "sceKeyboardDeviceOpen:\n");
+
+asm(".global sceKeyboardDisconnectDevice\n"
+    ".type sceKeyboardDisconnectDevice @function\n"
+    "sceKeyboardDisconnectDevice:\n");
+
+asm(".global sceKeyboardDisconnectPort\n"
+    ".type sceKeyboardDisconnectPort @function\n"
+    "sceKeyboardDisconnectPort:\n");
+
+asm(".global sceKeyboardGetConnection\n"
+    ".type sceKeyboardGetConnection @function\n"
+    "sceKeyboardGetConnection:\n");
+
+asm(".global sceKeyboardGetDeviceInfo\n"
+    ".type sceKeyboardGetDeviceInfo @function\n"
+    "sceKeyboardGetDeviceInfo:\n");
+
+asm(".global sceKeyboardGetKey2Char\n"
+    ".type sceKeyboardGetKey2Char @function\n"
+    "sceKeyboardGetKey2Char:\n");
+
+asm(".global sceKeyboardInit\n"
+    ".type sceKeyboardInit @function\n"
+    "sceKeyboardInit:\n");
+
+asm(".global sceKeyboardMbusInit\n"
+    ".type sceKeyboardMbusInit @function\n"
+    "sceKeyboardMbusInit:\n");
+
+asm(".global sceKeyboardOpen\n"
+    ".type sceKeyboardOpen @function\n"
+    "sceKeyboardOpen:\n");
+
+asm(".global sceKeyboardRead\n"
+    ".type sceKeyboardRead @function\n"
+    "sceKeyboardRead:\n");
+
+asm(".global sceKeyboardReadState\n"
+    ".type sceKeyboardReadState @function\n"
+    "sceKeyboardReadState:\n");
+
+asm(".global sceKeyboardSetProcessFocus\n"
+    ".type sceKeyboardSetProcessFocus @function\n"
+    "sceKeyboardSetProcessFocus:\n");
+
+asm(".global sceKeyboardSetProcessPrivilege\n"
+    ".type sceKeyboardSetProcessPrivilege @function\n"
+    "sceKeyboardSetProcessPrivilege:\n");
