b6fd73e5fdf0ef2a2507741157cd2881655cf849
John Törnblom <john.tornblom@gmail.com>
2024-07-07 18:09:08 +0200

    add stub for libSceRegMgr

---
+++ sce_stubs/libSceRegMgr.c
@@ -0,0 +1,256 @@
+
+asm(".global sceRegMgrBackupNeedMem\n"
+    ".type sceRegMgrBackupNeedMem @function\n"
+    "sceRegMgrBackupNeedMem:\n");
+
+asm(".global sceRegMgrBackupNeedMemForPS4\n"
+    ".type sceRegMgrBackupNeedMemForPS4 @function\n"
+    "sceRegMgrBackupNeedMemForPS4:\n");
+
+asm(".global sceRegMgrBackupPullData\n"
+    ".type sceRegMgrBackupPullData @function\n"
+    "sceRegMgrBackupPullData:\n");
+
+asm(".global sceRegMgrBackupPullDataForPS4\n"
+    ".type sceRegMgrBackupPullDataForPS4 @function\n"
+    "sceRegMgrBackupPullDataForPS4:\n");
+
+asm(".global sceRegMgrBackupPushData\n"
+    ".type sceRegMgrBackupPushData @function\n"
+    "sceRegMgrBackupPushData:\n");
+
+asm(".global sceRegMgrBackupPushDataForPS4\n"
+    ".type sceRegMgrBackupPushDataForPS4 @function\n"
+    "sceRegMgrBackupPushDataForPS4:\n");
+
+asm(".global sceRegMgrCheckError\n"
+    ".type sceRegMgrCheckError @function\n"
+    "sceRegMgrCheckError:\n");
+
+asm(".global sceRegMgrCheckIntVal\n"
+    ".type sceRegMgrCheckIntVal @function\n"
+    "sceRegMgrCheckIntVal:\n");
+
+asm(".global sceRegMgrCntSetInitReinst\n"
+    ".type sceRegMgrCntSetInitReinst @function\n"
+    "sceRegMgrCntSetInitReinst:\n");
+
+asm(".global sceRegMgrCntlDeleteReg\n"
+    ".type sceRegMgrCntlDeleteReg @function\n"
+    "sceRegMgrCntlDeleteReg:\n");
+
+asm(".global sceRegMgrCntlGetFileName\n"
+    ".type sceRegMgrCntlGetFileName @function\n"
+    "sceRegMgrCntlGetFileName:\n");
+
+asm(".global sceRegMgrCntlGetFilesCount\n"
+    ".type sceRegMgrCntlGetFilesCount @function\n"
+    "sceRegMgrCntlGetFilesCount:\n");
+
+asm(".global sceRegMgrCntlStart\n"
+    ".type sceRegMgrCntlStart @function\n"
+    "sceRegMgrCntlStart:\n");
+
+asm(".global sceRegMgrCntlStopHdd\n"
+    ".type sceRegMgrCntlStopHdd @function\n"
+    "sceRegMgrCntlStopHdd:\n");
+
+asm(".global sceRegMgrDrvDataCheckGet\n"
+    ".type sceRegMgrDrvDataCheckGet @function\n"
+    "sceRegMgrDrvDataCheckGet:\n");
+
+asm(".global sceRegMgrDrvDataClose\n"
+    ".type sceRegMgrDrvDataClose @function\n"
+    "sceRegMgrDrvDataClose:\n");
+
+asm(".global sceRegMgrDrvDataOpen\n"
+    ".type sceRegMgrDrvDataOpen @function\n"
+    "sceRegMgrDrvDataOpen:\n");
+
+asm(".global sceRegMgrDrvGetEntCnt\n"
+    ".type sceRegMgrDrvGetEntCnt @function\n"
+    "sceRegMgrDrvGetEntCnt:\n");
+
+asm(".global sceRegMgrDrvGetUpdateCnt\n"
+    ".type sceRegMgrDrvGetUpdateCnt @function\n"
+    "sceRegMgrDrvGetUpdateCnt:\n");
+
+asm(".global sceRegMgrEvtGetCnt\n"
+    ".type sceRegMgrEvtGetCnt @function\n"
+    "sceRegMgrEvtGetCnt:\n");
+
+asm(".global sceRegMgrEvtGetCntForPS4\n"
+    ".type sceRegMgrEvtGetCntForPS4 @function\n"
+    "sceRegMgrEvtGetCntForPS4:\n");
+
+asm(".global sceRegMgrEvtGetRegId\n"
+    ".type sceRegMgrEvtGetRegId @function\n"
+    "sceRegMgrEvtGetRegId:\n");
+
+asm(".global sceRegMgrEvtGetRegIdForPS4\n"
+    ".type sceRegMgrEvtGetRegIdForPS4 @function\n"
+    "sceRegMgrEvtGetRegIdForPS4:\n");
+
+asm(".global sceRegMgrGetBin\n"
+    ".type sceRegMgrGetBin @function\n"
+    "sceRegMgrGetBin:\n");
+
+asm(".global sceRegMgrGetBinInitVal\n"
+    ".type sceRegMgrGetBinInitVal @function\n"
+    "sceRegMgrGetBinInitVal:\n");
+
+asm(".global sceRegMgrGetInitError\n"
+    ".type sceRegMgrGetInitError @function\n"
+    "sceRegMgrGetInitError:\n");
+
+asm(".global sceRegMgrGetInt\n"
+    ".type sceRegMgrGetInt @function\n"
+    "sceRegMgrGetInt:\n");
+
+asm(".global sceRegMgrGetIntInitVal\n"
+    ".type sceRegMgrGetIntInitVal @function\n"
+    "sceRegMgrGetIntInitVal:\n");
+
+asm(".global sceRegMgrGetStr\n"
+    ".type sceRegMgrGetStr @function\n"
+    "sceRegMgrGetStr:\n");
+
+asm(".global sceRegMgrGetStrInitVal\n"
+    ".type sceRegMgrGetStrInitVal @function\n"
+    "sceRegMgrGetStrInitVal:\n");
+
+asm(".global sceRegMgrGetVersion\n"
+    ".type sceRegMgrGetVersion @function\n"
+    "sceRegMgrGetVersion:\n");
+
+asm(".global sceRegMgrGetVersionForPS4\n"
+    ".type sceRegMgrGetVersionForPS4 @function\n"
+    "sceRegMgrGetVersionForPS4:\n");
+
+asm(".global sceRegMgrIsChange\n"
+    ".type sceRegMgrIsChange @function\n"
+    "sceRegMgrIsChange:\n");
+
+asm(".global sceRegMgrIsInitOK\n"
+    ".type sceRegMgrIsInitOK @function\n"
+    "sceRegMgrIsInitOK:\n");
+
+asm(".global sceRegMgrLogPull\n"
+    ".type sceRegMgrLogPull @function\n"
+    "sceRegMgrLogPull:\n");
+
+asm(".global sceRegMgrLogStart\n"
+    ".type sceRegMgrLogStart @function\n"
+    "sceRegMgrLogStart:\n");
+
+asm(".global sceRegMgrMemset\n"
+    ".type sceRegMgrMemset @function\n"
+    "sceRegMgrMemset:\n");
+
+asm(".global sceRegMgrNonSysCheckError\n"
+    ".type sceRegMgrNonSysCheckError @function\n"
+    "sceRegMgrNonSysCheckError:\n");
+
+asm(".global sceRegMgrNonSysGetBin\n"
+    ".type sceRegMgrNonSysGetBin @function\n"
+    "sceRegMgrNonSysGetBin:\n");
+
+asm(".global sceRegMgrNonSysGetInt\n"
+    ".type sceRegMgrNonSysGetInt @function\n"
+    "sceRegMgrNonSysGetInt:\n");
+
+asm(".global sceRegMgrNonSysGetStr\n"
+    ".type sceRegMgrNonSysGetStr @function\n"
+    "sceRegMgrNonSysGetStr:\n");
+
+asm(".global sceRegMgrNonSysSetBin\n"
+    ".type sceRegMgrNonSysSetBin @function\n"
+    "sceRegMgrNonSysSetBin:\n");
+
+asm(".global sceRegMgrNonSysSetInt\n"
+    ".type sceRegMgrNonSysSetInt @function\n"
+    "sceRegMgrNonSysSetInt:\n");
+
+asm(".global sceRegMgrNonSysSetStr\n"
+    ".type sceRegMgrNonSysSetStr @function\n"
+    "sceRegMgrNonSysSetStr:\n");
+
+asm(".global sceRegMgrPrintInfo\n"
+    ".type sceRegMgrPrintInfo @function\n"
+    "sceRegMgrPrintInfo:\n");
+
+asm(".global sceRegMgrRecoverRegNvs\n"
+    ".type sceRegMgrRecoverRegNvs @function\n"
+    "sceRegMgrRecoverRegNvs:\n");
+
+asm(".global sceRegMgrResetVal\n"
+    ".type sceRegMgrResetVal @function\n"
+    "sceRegMgrResetVal:\n");
+
+asm(".global sceRegMgrSetBin\n"
+    ".type sceRegMgrSetBin @function\n"
+    "sceRegMgrSetBin:\n");
+
+asm(".global sceRegMgrSetInitLevel\n"
+    ".type sceRegMgrSetInitLevel @function\n"
+    "sceRegMgrSetInitLevel:\n");
+
+asm(".global sceRegMgrSetInt\n"
+    ".type sceRegMgrSetInt @function\n"
+    "sceRegMgrSetInt:\n");
+
+asm(".global sceRegMgrSetStr\n"
+    ".type sceRegMgrSetStr @function\n"
+    "sceRegMgrSetStr:\n");
+
+asm(".global sceRegMgrSetbin\n"
+    ".type sceRegMgrSetbin @function\n"
+    "sceRegMgrSetbin:\n");
+
+asm(".global sceRegMgrSrvCnvRegionInt\n"
+    ".type sceRegMgrSrvCnvRegionInt @function\n"
+    "sceRegMgrSrvCnvRegionInt:\n");
+
+asm(".global sceRegMgrSrvCnvRegionStr\n"
+    ".type sceRegMgrSrvCnvRegionStr @function\n"
+    "sceRegMgrSrvCnvRegionStr:\n");
+
+asm(".global sceRegMgrSrvGetMachineType\n"
+    ".type sceRegMgrSrvGetMachineType @function\n"
+    "sceRegMgrSrvGetMachineType:\n");
+
+asm(".global sceRegMgrSrvGetQAFforReg\n"
+    ".type sceRegMgrSrvGetQAFforReg @function\n"
+    "sceRegMgrSrvGetQAFforReg:\n");
+
+asm(".global sceRegMgrSrvGetRealMachineType\n"
+    ".type sceRegMgrSrvGetRealMachineType @function\n"
+    "sceRegMgrSrvGetRealMachineType:\n");
+
+asm(".global sceRegMgrSrvGetRegion\n"
+    ".type sceRegMgrSrvGetRegion @function\n"
+    "sceRegMgrSrvGetRegion:\n");
+
+asm(".global sceRegMgrSrvGetRegionStr\n"
+    ".type sceRegMgrSrvGetRegionStr @function\n"
+    "sceRegMgrSrvGetRegionStr:\n");
+
+asm(".global sceRegMgrStrncpy\n"
+    ".type sceRegMgrStrncpy @function\n"
+    "sceRegMgrStrncpy:\n");
+
+asm(".global sceRegMgrToolDataCheckGet\n"
+    ".type sceRegMgrToolDataCheckGet @function\n"
+    "sceRegMgrToolDataCheckGet:\n");
+
+asm(".global sceRegMgrToolGetEntryCnt\n"
+    ".type sceRegMgrToolGetEntryCnt @function\n"
+    "sceRegMgrToolGetEntryCnt:\n");
+
+asm(".global sceRegMgrToolGetInfo\n"
+    ".type sceRegMgrToolGetInfo @function\n"
+    "sceRegMgrToolGetInfo:\n");
+
+asm(".global sceRegMgrToolGetUpdateCnt\n"
+    ".type sceRegMgrToolGetUpdateCnt @function\n"
+    "sceRegMgrToolGetUpdateCnt:\n");
