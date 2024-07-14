7d6eabf1bd09fce7017fc3f764eea3b57831a440
John Törnblom <john.tornblom@gmail.com>
2024-07-15 01:05:28 +0200

    sce stubs: add libSceImeDialog

---
+++ sce_stubs/libSceImeDialog.c
@@ -0,0 +1,56 @@
+
+asm(".global sceImeDialogAbort\n"
+    ".type sceImeDialogAbort @function\n"
+    "sceImeDialogAbort:\n");
+
+asm(".global sceImeDialogForTestFunction\n"
+    ".type sceImeDialogForTestFunction @function\n"
+    "sceImeDialogForTestFunction:\n");
+
+asm(".global sceImeDialogForceClose\n"
+    ".type sceImeDialogForceClose @function\n"
+    "sceImeDialogForceClose:\n");
+
+asm(".global sceImeDialogGetCurrentStarState\n"
+    ".type sceImeDialogGetCurrentStarState @function\n"
+    "sceImeDialogGetCurrentStarState:\n");
+
+asm(".global sceImeDialogGetPanelPositionAndForm\n"
+    ".type sceImeDialogGetPanelPositionAndForm @function\n"
+    "sceImeDialogGetPanelPositionAndForm:\n");
+
+asm(".global sceImeDialogGetPanelSize\n"
+    ".type sceImeDialogGetPanelSize @function\n"
+    "sceImeDialogGetPanelSize:\n");
+
+asm(".global sceImeDialogGetPanelSizeExtended\n"
+    ".type sceImeDialogGetPanelSizeExtended @function\n"
+    "sceImeDialogGetPanelSizeExtended:\n");
+
+asm(".global sceImeDialogGetResult\n"
+    ".type sceImeDialogGetResult @function\n"
+    "sceImeDialogGetResult:\n");
+
+asm(".global sceImeDialogGetStatus\n"
+    ".type sceImeDialogGetStatus @function\n"
+    "sceImeDialogGetStatus:\n");
+
+asm(".global sceImeDialogInit\n"
+    ".type sceImeDialogInit @function\n"
+    "sceImeDialogInit:\n");
+
+asm(".global sceImeDialogInitInternal\n"
+    ".type sceImeDialogInitInternal @function\n"
+    "sceImeDialogInitInternal:\n");
+
+asm(".global sceImeDialogInitInternalWithSubInfo\n"
+    ".type sceImeDialogInitInternalWithSubInfo @function\n"
+    "sceImeDialogInitInternalWithSubInfo:\n");
+
+asm(".global sceImeDialogSetPanelPosition\n"
+    ".type sceImeDialogSetPanelPosition @function\n"
+    "sceImeDialogSetPanelPosition:\n");
+
+asm(".global sceImeDialogTerm\n"
+    ".type sceImeDialogTerm @function\n"
+    "sceImeDialogTerm:\n");
