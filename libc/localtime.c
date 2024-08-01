7886bcd6f62539de4eea52eb040b2fb6c8494a85
John Törnblom <john.tornblom@gmail.com>
2024-08-01 13:44:34 +0200

    libc: add localtime_r()

---
+++ libc/localtime.c
@@ -0,0 +1,27 @@
+/* Copyright (C) 2024 John Törnblom
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 3, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; see the file COPYING. If not, see
+<http://www.gnu.org/licenses/>.  */
+
+#include <time.h>
+
+struct tm* localtime_s(const time_t*, struct tm*);
+
+
+struct tm*
+localtime_r(const time_t *restrict timep, struct tm *restrict result) {
+  return localtime_s(timep, result);
+}
+
+  
