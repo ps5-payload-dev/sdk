96d96787cf1a278ff2902ec9ef6cf3cd955caf46
John Törnblom <john.tornblom@gmail.com>
2024-08-21 13:30:27 +0200

    libc: add stubs for getpwnam_r and getpwuid_r

--- libc/passwd.c
+++ libc/passwd.c
@@ -28,3 +28,17 @@ struct passwd*
 getpwuid(uid_t uid) {
   return 0;
 }
+
+int
+getpwnam_r(const char *name, struct passwd *pwd, char *buf, size_t buflen,
+           struct passwd **result) {
+    *result = 0;
+    return 0;
+}
+
+int
+getpwuid_r(uid_t uid, struct passwd *pwd, char *buf, size_t buflen,
+           struct passwd **result) {
+    *result = 0;
+    return 0;
+}
