849d590d219b37d01025175af637864fe4280753
John Törnblom <john.tornblom@gmail.com>
2024-10-26 21:42:56 +0200

    cosmetics

--- host/win/prospero-clang++.cmd
+++ host/win/prospero-clang++.cmd
@@ -9,7 +9,7 @@ set LIBS_CXX=-lunwind -lc++abi -lc++
 set LIBS_DEPS=-lSceLibcInternal -lSceNet
 set INCDIR=%PS5_PAYLOAD_SDK%\target\include\c++\v1
 
-set PATH=%PATH%;%PS5_PAYLOAD_SDK%/win
+set PATH=%PATH%;%PS5_PAYLOAD_SDK%\win
 
 clang++ -target x86_64-sie-ps5 -isysroot "%PS5_PAYLOAD_SDK%" -stdlib++-isystem "%INCDIR%" ^
     -fno-stack-protector -fno-plt -femulated-tls -frtti -fexceptions ^
--- host/win/prospero-clang.cmd
+++ host/win/prospero-clang.cmd
@@ -8,7 +8,8 @@ set LIBS_C=-lc
 set LIBS_DEPS=-lSceLibcInternal -lSceNet
 set INCDIR=%PS5_PAYLOAD_SDK%\target\include
 
-set PATH=%PATH%;%PS5_PAYLOAD_SDK%/win
+set PATH=%PATH%;%PS5_PAYLOAD_SDK%\win
+
 
 clang -target x86_64-sie-ps5 -isysroot "%PS5_PAYLOAD_SDK%" -isystem "%INCDIR%" ^
     -fno-stack-protector -fno-plt -femulated-tls -frtti -fexceptions ^
