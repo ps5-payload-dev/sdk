4cdfbd465bf6356c14b3ceb19d44b154482dde34
John Törnblom <john.tornblom@gmail.com>
2024-10-26 23:41:55 +0200

    win: add cmake utility

---
+++ host/win/prospero-cmake.cmd
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+
+set SCRIPT_PATH=%~dp0
+set PS5_PAYLOAD_SDK=%SCRIPT_PATH%..
+
+set TOOLCHAIN=%PS5_PAYLOAD_SDK%\toolchain\prospero.cmake
+set PATH=%PATH%;%PS5_PAYLOAD_SDK%\win
+
+set BUILD_SYS_ARGS=-G Ninja
+:checkargs
+    if "%1"=="" goto runcmd
+    else if "%1"=="-G" (
+        set BUILD_SYS_ARGS=""
+    )
+shift
+goto checkargs
+
+:runcmd
+cmake -DCMAKE_TOOLCHAIN_FILE="%TOOLCHAIN%" %* %BUILD_SYS_ARGS%
