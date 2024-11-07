580866d91eaa95941bdff2d624b70c9e89053f7b
John Törnblom <john.tornblom@gmail.com>
2024-11-07 19:11:20 +0100

    libc: add a couple of stubs for functions that currently does not work on ps5

---
+++ libc/grp.c
@@ -0,0 +1,33 @@
+/* Copyright (C) 2024 John Törnblom
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 3, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; see the file COPYING. If not, see
+<http://www.gnu.org/licenses/>.  */
+
+#include <sys/types.h>
+#include <grp.h>
+
+
+struct group*
+getgrnam(const char *name) {
+#warning "getgrnam() not implemented"
+  return 0;
+}
+
+
+struct group*
+getgrgid(gid_t gid) {
+#warning "getgrgid() not implemented"
+  return 0;
+}
+
