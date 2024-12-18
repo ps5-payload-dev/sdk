6dd4ad3b2ba790c18a9b8f5e2af3c94960e2e1c3
John Törnblom <john.tornblom@gmail.com>
2024-12-18 19:49:15 +0100

    sce_stubs: add libSceRemoteplay

---
+++ sce_stubs/libSceRemoteplay.c
@@ -0,0 +1,172 @@
+
+asm(".global sceRemoteplayApprove\n"
+    ".type sceRemoteplayApprove @function\n"
+    "sceRemoteplayApprove:\n");
+
+asm(".global sceRemoteplayClearAllRegistData\n"
+    ".type sceRemoteplayClearAllRegistData @function\n"
+    "sceRemoteplayClearAllRegistData:\n");
+
+asm(".global sceRemoteplayClearConnectHistory\n"
+    ".type sceRemoteplayClearConnectHistory @function\n"
+    "sceRemoteplayClearConnectHistory:\n");
+
+asm(".global sceRemoteplayConfirmDeviceRegist\n"
+    ".type sceRemoteplayConfirmDeviceRegist @function\n"
+    "sceRemoteplayConfirmDeviceRegist:\n");
+
+asm(".global sceRemoteplayDisconnect\n"
+    ".type sceRemoteplayDisconnect @function\n"
+    "sceRemoteplayDisconnect:\n");
+
+asm(".global sceRemoteplayGeneratePinCode\n"
+    ".type sceRemoteplayGeneratePinCode @function\n"
+    "sceRemoteplayGeneratePinCode:\n");
+
+asm(".global sceRemoteplayGetApMode\n"
+    ".type sceRemoteplayGetApMode @function\n"
+    "sceRemoteplayGetApMode:\n");
+
+asm(".global sceRemoteplayGetConnectHistory\n"
+    ".type sceRemoteplayGetConnectHistory @function\n"
+    "sceRemoteplayGetConnectHistory:\n");
+
+asm(".global sceRemoteplayGetConnectUserId\n"
+    ".type sceRemoteplayGetConnectUserId @function\n"
+    "sceRemoteplayGetConnectUserId:\n");
+
+asm(".global sceRemoteplayGetConnectionStatus\n"
+    ".type sceRemoteplayGetConnectionStatus @function\n"
+    "sceRemoteplayGetConnectionStatus:\n");
+
+asm(".global sceRemoteplayGetMbusDeviceInfo\n"
+    ".type sceRemoteplayGetMbusDeviceInfo @function\n"
+    "sceRemoteplayGetMbusDeviceInfo:\n");
+
+asm(".global sceRemoteplayGetOperationStatus\n"
+    ".type sceRemoteplayGetOperationStatus @function\n"
+    "sceRemoteplayGetOperationStatus:\n");
+
+asm(".global sceRemoteplayGetPrimaryUserId\n"
+    ".type sceRemoteplayGetPrimaryUserId @function\n"
+    "sceRemoteplayGetPrimaryUserId:\n");
+
+asm(".global sceRemoteplayGetRemoteplayStatus\n"
+    ".type sceRemoteplayGetRemoteplayStatus @function\n"
+    "sceRemoteplayGetRemoteplayStatus:\n");
+
+asm(".global sceRemoteplayGetRpMode\n"
+    ".type sceRemoteplayGetRpMode @function\n"
+    "sceRemoteplayGetRpMode:\n");
+
+asm(".global sceRemoteplayImeClose\n"
+    ".type sceRemoteplayImeClose @function\n"
+    "sceRemoteplayImeClose:\n");
+
+asm(".global sceRemoteplayImeFilterResult\n"
+    ".type sceRemoteplayImeFilterResult @function\n"
+    "sceRemoteplayImeFilterResult:\n");
+
+asm(".global sceRemoteplayImeGetEvent\n"
+    ".type sceRemoteplayImeGetEvent @function\n"
+    "sceRemoteplayImeGetEvent:\n");
+
+asm(".global sceRemoteplayImeNotify\n"
+    ".type sceRemoteplayImeNotify @function\n"
+    "sceRemoteplayImeNotify:\n");
+
+asm(".global sceRemoteplayImeNotifyEventResult\n"
+    ".type sceRemoteplayImeNotifyEventResult @function\n"
+    "sceRemoteplayImeNotifyEventResult:\n");
+
+asm(".global sceRemoteplayImeOpen\n"
+    ".type sceRemoteplayImeOpen @function\n"
+    "sceRemoteplayImeOpen:\n");
+
+asm(".global sceRemoteplayImeSetCaret\n"
+    ".type sceRemoteplayImeSetCaret @function\n"
+    "sceRemoteplayImeSetCaret:\n");
+
+asm(".global sceRemoteplayImeSetText\n"
+    ".type sceRemoteplayImeSetText @function\n"
+    "sceRemoteplayImeSetText:\n");
+
+asm(".global sceRemoteplayInitialize\n"
+    ".type sceRemoteplayInitialize @function\n"
+    "sceRemoteplayInitialize:\n");
+
+asm(".global sceRemoteplayIsRemoteOskReady\n"
+    ".type sceRemoteplayIsRemoteOskReady @function\n"
+    "sceRemoteplayIsRemoteOskReady:\n");
+
+asm(".global sceRemoteplayIsRemotePlaying\n"
+    ".type sceRemoteplayIsRemotePlaying @function\n"
+    "sceRemoteplayIsRemotePlaying:\n");
+
+asm(".global sceRemoteplayNotifyCanWakeup\n"
+    ".type sceRemoteplayNotifyCanWakeup @function\n"
+    "sceRemoteplayNotifyCanWakeup:\n");
+
+asm(".global sceRemoteplayNotifyMbusDeviceRegistComplete\n"
+    ".type sceRemoteplayNotifyMbusDeviceRegistComplete @function\n"
+    "sceRemoteplayNotifyMbusDeviceRegistComplete:\n");
+
+asm(".global sceRemoteplayNotifyNpPushWakeup\n"
+    ".type sceRemoteplayNotifyNpPushWakeup @function\n"
+    "sceRemoteplayNotifyNpPushWakeup:\n");
+
+asm(".global sceRemoteplayNotifyPinCodeError\n"
+    ".type sceRemoteplayNotifyPinCodeError @function\n"
+    "sceRemoteplayNotifyPinCodeError:\n");
+
+asm(".global sceRemoteplayNotifySubmitCode\n"
+    ".type sceRemoteplayNotifySubmitCode @function\n"
+    "sceRemoteplayNotifySubmitCode:\n");
+
+asm(".global sceRemoteplayNotifyUserDelete\n"
+    ".type sceRemoteplayNotifyUserDelete @function\n"
+    "sceRemoteplayNotifyUserDelete:\n");
+
+asm(".global sceRemoteplayPrintAllRegistData\n"
+    ".type sceRemoteplayPrintAllRegistData @function\n"
+    "sceRemoteplayPrintAllRegistData:\n");
+
+asm(".global sceRemoteplayProhibit\n"
+    ".type sceRemoteplayProhibit @function\n"
+    "sceRemoteplayProhibit:\n");
+
+asm(".global sceRemoteplayProhibitStreaming\n"
+    ".type sceRemoteplayProhibitStreaming @function\n"
+    "sceRemoteplayProhibitStreaming:\n");
+
+asm(".global sceRemoteplayServerLock\n"
+    ".type sceRemoteplayServerLock @function\n"
+    "sceRemoteplayServerLock:\n");
+
+asm(".global sceRemoteplayServerUnLock\n"
+    ".type sceRemoteplayServerUnLock @function\n"
+    "sceRemoteplayServerUnLock:\n");
+
+asm(".global sceRemoteplaySetApMode\n"
+    ".type sceRemoteplaySetApMode @function\n"
+    "sceRemoteplaySetApMode:\n");
+
+asm(".global sceRemoteplaySetLogLevel\n"
+    ".type sceRemoteplaySetLogLevel @function\n"
+    "sceRemoteplaySetLogLevel:\n");
+
+asm(".global sceRemoteplaySetProhibition\n"
+    ".type sceRemoteplaySetProhibition @function\n"
+    "sceRemoteplaySetProhibition:\n");
+
+asm(".global sceRemoteplaySetProhibitionForVsh\n"
+    ".type sceRemoteplaySetProhibitionForVsh @function\n"
+    "sceRemoteplaySetProhibitionForVsh:\n");
+
+asm(".global sceRemoteplaySetRpMode\n"
+    ".type sceRemoteplaySetRpMode @function\n"
+    "sceRemoteplaySetRpMode:\n");
+
+asm(".global sceRemoteplayTerminate\n"
+    ".type sceRemoteplayTerminate @function\n"
+    "sceRemoteplayTerminate:\n");
