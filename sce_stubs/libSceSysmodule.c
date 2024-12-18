2c04d28a521a7c27ecff59bebd6dcedccdb6457f
John Törnblom <john.tornblom@gmail.com>
2024-12-18 20:26:38 +0100

    sce_stubs: add libSceSysmodule

---
+++ sce_stubs/libSceSysmodule.c
@@ -0,0 +1,64 @@
+
+asm(".global sceSysmoduleGetModuleHandleInternal\n"
+    ".type sceSysmoduleGetModuleHandleInternal @function\n"
+    "sceSysmoduleGetModuleHandleInternal:\n");
+
+asm(".global sceSysmoduleGetModuleInfoForUnwind\n"
+    ".type sceSysmoduleGetModuleInfoForUnwind @function\n"
+    "sceSysmoduleGetModuleInfoForUnwind:\n");
+
+asm(".global sceSysmoduleIsCalledFromSysModule\n"
+    ".type sceSysmoduleIsCalledFromSysModule @function\n"
+    "sceSysmoduleIsCalledFromSysModule:\n");
+
+asm(".global sceSysmoduleIsCameraPreloaded\n"
+    ".type sceSysmoduleIsCameraPreloaded @function\n"
+    "sceSysmoduleIsCameraPreloaded:\n");
+
+asm(".global sceSysmoduleIsLoaded\n"
+    ".type sceSysmoduleIsLoaded @function\n"
+    "sceSysmoduleIsLoaded:\n");
+
+asm(".global sceSysmoduleIsLoadedInternal\n"
+    ".type sceSysmoduleIsLoadedInternal @function\n"
+    "sceSysmoduleIsLoadedInternal:\n");
+
+asm(".global sceSysmoduleLoadModule\n"
+    ".type sceSysmoduleLoadModule @function\n"
+    "sceSysmoduleLoadModule:\n");
+
+asm(".global sceSysmoduleLoadModuleByNameInternal\n"
+    ".type sceSysmoduleLoadModuleByNameInternal @function\n"
+    "sceSysmoduleLoadModuleByNameInternal:\n");
+
+asm(".global sceSysmoduleLoadModuleInternal\n"
+    ".type sceSysmoduleLoadModuleInternal @function\n"
+    "sceSysmoduleLoadModuleInternal:\n");
+
+asm(".global sceSysmoduleLoadModuleInternalWithArg\n"
+    ".type sceSysmoduleLoadModuleInternalWithArg @function\n"
+    "sceSysmoduleLoadModuleInternalWithArg:\n");
+
+asm(".global sceSysmoduleMapLibcForLibkernel\n"
+    ".type sceSysmoduleMapLibcForLibkernel @function\n"
+    "sceSysmoduleMapLibcForLibkernel:\n");
+
+asm(".global sceSysmodulePreloadModuleForLibkernel\n"
+    ".type sceSysmodulePreloadModuleForLibkernel @function\n"
+    "sceSysmodulePreloadModuleForLibkernel:\n");
+
+asm(".global sceSysmoduleUnloadModule\n"
+    ".type sceSysmoduleUnloadModule @function\n"
+    "sceSysmoduleUnloadModule:\n");
+
+asm(".global sceSysmoduleUnloadModuleByNameInternal\n"
+    ".type sceSysmoduleUnloadModuleByNameInternal @function\n"
+    "sceSysmoduleUnloadModuleByNameInternal:\n");
+
+asm(".global sceSysmoduleUnloadModuleInternal\n"
+    ".type sceSysmoduleUnloadModuleInternal @function\n"
+    "sceSysmoduleUnloadModuleInternal:\n");
+
+asm(".global sceSysmoduleUnloadModuleInternalWithArg\n"
+    ".type sceSysmoduleUnloadModuleInternalWithArg @function\n"
+    "sceSysmoduleUnloadModuleInternalWithArg:\n");
