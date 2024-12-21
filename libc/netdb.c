be53a866218ac8d245416d92d1ab6b5852f456e0
John Törnblom <john.tornblom@gmail.com>
2024-12-21 18:16:15 +0100

    libc: add getservbyport()

--- libc/netdb.c
+++ libc/netdb.c
@@ -555,3 +555,83 @@ freeaddrinfo(struct addrinfo *res) {
   }
 }
 
+int
+getservbyport_r(int port, const char *prots, struct servent *se, char *buf,
+                size_t buflen, struct servent **res) {
+  struct sockaddr_in sin = {
+    .sin_family = AF_INET,
+    .sin_port = port,
+  };
+  int i;
+  int r;
+
+  if(!prots) {
+    if((r=getservbyport_r(port, "tcp", se, buf, buflen, res))) {
+      r = getservbyport_r(port, "udp", se, buf, buflen, res);
+    }
+    return r;
+  }
+  *res = 0;
+
+  // Align buffer
+  i = (uintptr_t)buf & sizeof(char *)-1;
+  if(!i) {
+    i = sizeof(char *);
+  }
+  if(buflen <= 3*sizeof(char *)-i) {
+    return ERANGE;
+  }
+  buf += sizeof(char *)-i;
+  buflen -= sizeof(char *)-i;
+
+  if(strcmp(prots, "tcp") && strcmp(prots, "udp")) {
+    return EINVAL;
+  }
+
+  se->s_port = port;
+  se->s_proto = (char *)prots;
+  se->s_aliases = (void *)buf;
+  buf += 2*sizeof(char *);
+  buflen -= 2*sizeof(char *);
+  se->s_aliases[1] = 0;
+  se->s_aliases[0] = se->s_name = buf;
+
+  switch(getnameinfo((void *)&sin, sizeof(sin), 0, 0, buf, buflen,
+                      strcmp(prots, "udp") ? 0 : NI_DGRAM)) {
+  case EAI_MEMORY:
+  case EAI_SYSTEM:
+    return ENOMEM;
+
+  case EAI_OVERFLOW:
+    return ERANGE;
+
+  default:
+    return ENOENT;
+
+  case 0:
+    break;
+  }
+
+  // A numeric port string is not a service record.
+  if(strtol(buf, 0, 10) == ntohs(port)) {
+    return ENOENT;
+  }
+
+  *res = se;
+
+  return 0;
+}
+
+
+struct servent*
+getservbyport(int port, const char *prots) {
+  static struct servent se;
+  static long buf[32/sizeof(long)];
+  struct servent *res;
+
+  if(getservbyport_r(port, prots, &se, (void *)buf, sizeof buf, &res)) {
+    return 0;
+  }
+
+  return &se;
+}
