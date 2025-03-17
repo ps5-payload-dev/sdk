ff491adf2daf1ef27229fb12f5216100b735bdf1
John Törnblom <john.tornblom@gmail.com>
2025-03-17 16:30:00 +0100

    libc: add asctime_r()

---
+++ libc/asctime_r.c
@@ -0,0 +1,34 @@
+/* Copyright (C) 2024 John Törnblom
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 3, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; see the file COPYING. If not, see
+<http://www.gnu.org/licenses/>.  */
+
+#include <time.h>
+
+
+int asctime_s(char*, size_t, const struct tm*);
+
+
+char*
+asctime_r(const struct tm *tm, char *buf) {
+  if(!tm || !buf) {
+    return 0;
+  }
+
+  if(asctime_s(buf, 26, tm)) {
+    return 0;
+  }
+
+  return buf;
+}
