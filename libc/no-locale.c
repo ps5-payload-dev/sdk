c096b8c5fd5ed0480a2dde36bf4e6f3c2620f8ab
John Törnblom <john.tornblom@gmail.com>
2025-12-31 16:29:18 +0100

    locale: use freebsd struct for localeconv

--- libc/no-locale.c
+++ libc/no-locale.c
@@ -15,6 +15,7 @@ along with this program; see the file COPYING. If not, see
 <http://www.gnu.org/licenses/>.  */
 
 #include <ctype.h>
+#include <limits.h>
 #include <locale.h>
 #include <stdarg.h>
 #include <stdio.h>
@@ -227,6 +228,40 @@ const _RuneLocale _DefaultRuneLocale = {
    0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff,
   },
 };
+
+
+static const struct lconv posix_lconv = {
+  .decimal_point = ".",
+  .thousands_sep = "",
+  .grouping = "",
+  .int_curr_symbol = "",
+  .currency_symbol = "",
+  .mon_decimal_point = "",
+  .mon_thousands_sep = "",
+  .mon_grouping = "",
+  .positive_sign = "",
+  .negative_sign = "",
+  .int_frac_digits = CHAR_MAX,
+  .frac_digits = CHAR_MAX,
+  .p_cs_precedes = CHAR_MAX,
+  .p_sep_by_space = CHAR_MAX,
+  .n_cs_precedes = CHAR_MAX,
+  .n_sep_by_space = CHAR_MAX,
+  .p_sign_posn = CHAR_MAX,
+  .n_sign_posn = CHAR_MAX,
+  .int_p_cs_precedes = CHAR_MAX,
+  .int_p_sep_by_space = CHAR_MAX,
+  .int_n_cs_precedes = CHAR_MAX,
+  .int_n_sep_by_space = CHAR_MAX,
+  .int_p_sign_posn = CHAR_MAX,
+  .int_n_sign_posn = CHAR_MAX,
+};
+
+
+struct lconv*
+localeconv(void) {
+  return (void*)&posix_lconv;
+}
 
 
 int
