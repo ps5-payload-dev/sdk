b58c5d94a99e171527a9b790a6d273ca126b0e6e
John Törnblom <john.tornblom@gmail.com>
2026-02-19 16:26:42 +0100

    stubs: add libSceHttp

---
+++ sce_stubs/libSceHttp.c
@@ -0,0 +1,460 @@
+
+asm(".global sceHttpAbortRequest\n"
+    ".type sceHttpAbortRequest @function\n"
+    "sceHttpAbortRequest:\n");
+
+asm(".global sceHttpAbortRequestForce\n"
+    ".type sceHttpAbortRequestForce @function\n"
+    "sceHttpAbortRequestForce:\n");
+
+asm(".global sceHttpAbortWaitRequest\n"
+    ".type sceHttpAbortWaitRequest @function\n"
+    "sceHttpAbortWaitRequest:\n");
+
+asm(".global sceHttpAddCookie\n"
+    ".type sceHttpAddCookie @function\n"
+    "sceHttpAddCookie:\n");
+
+asm(".global sceHttpAddQuery\n"
+    ".type sceHttpAddQuery @function\n"
+    "sceHttpAddQuery:\n");
+
+asm(".global sceHttpAddRequestHeader\n"
+    ".type sceHttpAddRequestHeader @function\n"
+    "sceHttpAddRequestHeader:\n");
+
+asm(".global sceHttpAddRequestHeaderRaw\n"
+    ".type sceHttpAddRequestHeaderRaw @function\n"
+    "sceHttpAddRequestHeaderRaw:\n");
+
+asm(".global sceHttpAuthCacheExport\n"
+    ".type sceHttpAuthCacheExport @function\n"
+    "sceHttpAuthCacheExport:\n");
+
+asm(".global sceHttpAuthCacheFlush\n"
+    ".type sceHttpAuthCacheFlush @function\n"
+    "sceHttpAuthCacheFlush:\n");
+
+asm(".global sceHttpAuthCacheImport\n"
+    ".type sceHttpAuthCacheImport @function\n"
+    "sceHttpAuthCacheImport:\n");
+
+asm(".global sceHttpCacheRedirectedConnectionEnabled\n"
+    ".type sceHttpCacheRedirectedConnectionEnabled @function\n"
+    "sceHttpCacheRedirectedConnectionEnabled:\n");
+
+asm(".global sceHttpCookieExport\n"
+    ".type sceHttpCookieExport @function\n"
+    "sceHttpCookieExport:\n");
+
+asm(".global sceHttpCookieFlush\n"
+    ".type sceHttpCookieFlush @function\n"
+    "sceHttpCookieFlush:\n");
+
+asm(".global sceHttpCookieImport\n"
+    ".type sceHttpCookieImport @function\n"
+    "sceHttpCookieImport:\n");
+
+asm(".global sceHttpCreateConnection\n"
+    ".type sceHttpCreateConnection @function\n"
+    "sceHttpCreateConnection:\n");
+
+asm(".global sceHttpCreateConnectionWithURL\n"
+    ".type sceHttpCreateConnectionWithURL @function\n"
+    "sceHttpCreateConnectionWithURL:\n");
+
+asm(".global sceHttpCreateEpoll\n"
+    ".type sceHttpCreateEpoll @function\n"
+    "sceHttpCreateEpoll:\n");
+
+asm(".global sceHttpCreateRequest\n"
+    ".type sceHttpCreateRequest @function\n"
+    "sceHttpCreateRequest:\n");
+
+asm(".global sceHttpCreateRequest2\n"
+    ".type sceHttpCreateRequest2 @function\n"
+    "sceHttpCreateRequest2:\n");
+
+asm(".global sceHttpCreateRequestWithURL\n"
+    ".type sceHttpCreateRequestWithURL @function\n"
+    "sceHttpCreateRequestWithURL:\n");
+
+asm(".global sceHttpCreateRequestWithURL2\n"
+    ".type sceHttpCreateRequestWithURL2 @function\n"
+    "sceHttpCreateRequestWithURL2:\n");
+
+asm(".global sceHttpCreateTemplate\n"
+    ".type sceHttpCreateTemplate @function\n"
+    "sceHttpCreateTemplate:\n");
+
+asm(".global sceHttpDbgEnableProfile\n"
+    ".type sceHttpDbgEnableProfile @function\n"
+    "sceHttpDbgEnableProfile:\n");
+
+asm(".global sceHttpDbgGetConnectionStat\n"
+    ".type sceHttpDbgGetConnectionStat @function\n"
+    "sceHttpDbgGetConnectionStat:\n");
+
+asm(".global sceHttpDbgGetRequestStat\n"
+    ".type sceHttpDbgGetRequestStat @function\n"
+    "sceHttpDbgGetRequestStat:\n");
+
+asm(".global sceHttpDbgSetPrintf\n"
+    ".type sceHttpDbgSetPrintf @function\n"
+    "sceHttpDbgSetPrintf:\n");
+
+asm(".global sceHttpDbgShowConnectionStat\n"
+    ".type sceHttpDbgShowConnectionStat @function\n"
+    "sceHttpDbgShowConnectionStat:\n");
+
+asm(".global sceHttpDbgShowMemoryPoolStat\n"
+    ".type sceHttpDbgShowMemoryPoolStat @function\n"
+    "sceHttpDbgShowMemoryPoolStat:\n");
+
+asm(".global sceHttpDbgShowRequestStat\n"
+    ".type sceHttpDbgShowRequestStat @function\n"
+    "sceHttpDbgShowRequestStat:\n");
+
+asm(".global sceHttpDbgShowStat\n"
+    ".type sceHttpDbgShowStat @function\n"
+    "sceHttpDbgShowStat:\n");
+
+asm(".global sceHttpDeleteConnection\n"
+    ".type sceHttpDeleteConnection @function\n"
+    "sceHttpDeleteConnection:\n");
+
+asm(".global sceHttpDeleteRequest\n"
+    ".type sceHttpDeleteRequest @function\n"
+    "sceHttpDeleteRequest:\n");
+
+asm(".global sceHttpDeleteTemplate\n"
+    ".type sceHttpDeleteTemplate @function\n"
+    "sceHttpDeleteTemplate:\n");
+
+asm(".global sceHttpDestroyEpoll\n"
+    ".type sceHttpDestroyEpoll @function\n"
+    "sceHttpDestroyEpoll:\n");
+
+asm(".global sceHttpGetAcceptEncodingGZIPEnabled\n"
+    ".type sceHttpGetAcceptEncodingGZIPEnabled @function\n"
+    "sceHttpGetAcceptEncodingGZIPEnabled:\n");
+
+asm(".global sceHttpGetAllResponseHeaders\n"
+    ".type sceHttpGetAllResponseHeaders @function\n"
+    "sceHttpGetAllResponseHeaders:\n");
+
+asm(".global sceHttpGetAuthEnabled\n"
+    ".type sceHttpGetAuthEnabled @function\n"
+    "sceHttpGetAuthEnabled:\n");
+
+asm(".global sceHttpGetAutoRedirect\n"
+    ".type sceHttpGetAutoRedirect @function\n"
+    "sceHttpGetAutoRedirect:\n");
+
+asm(".global sceHttpGetConnectionStat\n"
+    ".type sceHttpGetConnectionStat @function\n"
+    "sceHttpGetConnectionStat:\n");
+
+asm(".global sceHttpGetCookie\n"
+    ".type sceHttpGetCookie @function\n"
+    "sceHttpGetCookie:\n");
+
+asm(".global sceHttpGetCookieEnabled\n"
+    ".type sceHttpGetCookieEnabled @function\n"
+    "sceHttpGetCookieEnabled:\n");
+
+asm(".global sceHttpGetCookieStats\n"
+    ".type sceHttpGetCookieStats @function\n"
+    "sceHttpGetCookieStats:\n");
+
+asm(".global sceHttpGetEpoll\n"
+    ".type sceHttpGetEpoll @function\n"
+    "sceHttpGetEpoll:\n");
+
+asm(".global sceHttpGetEpollId\n"
+    ".type sceHttpGetEpollId @function\n"
+    "sceHttpGetEpollId:\n");
+
+asm(".global sceHttpGetLastErrno\n"
+    ".type sceHttpGetLastErrno @function\n"
+    "sceHttpGetLastErrno:\n");
+
+asm(".global sceHttpGetMemoryPoolStats\n"
+    ".type sceHttpGetMemoryPoolStats @function\n"
+    "sceHttpGetMemoryPoolStats:\n");
+
+asm(".global sceHttpGetNonblock\n"
+    ".type sceHttpGetNonblock @function\n"
+    "sceHttpGetNonblock:\n");
+
+asm(".global sceHttpGetRegisteredCtxIds\n"
+    ".type sceHttpGetRegisteredCtxIds @function\n"
+    "sceHttpGetRegisteredCtxIds:\n");
+
+asm(".global sceHttpGetResponseContentLength\n"
+    ".type sceHttpGetResponseContentLength @function\n"
+    "sceHttpGetResponseContentLength:\n");
+
+asm(".global sceHttpGetStatusCode\n"
+    ".type sceHttpGetStatusCode @function\n"
+    "sceHttpGetStatusCode:\n");
+
+asm(".global sceHttpInit\n"
+    ".type sceHttpInit @function\n"
+    "sceHttpInit:\n");
+
+asm(".global sceHttpParseResponseHeader\n"
+    ".type sceHttpParseResponseHeader @function\n"
+    "sceHttpParseResponseHeader:\n");
+
+asm(".global sceHttpParseStatusLine\n"
+    ".type sceHttpParseStatusLine @function\n"
+    "sceHttpParseStatusLine:\n");
+
+asm(".global sceHttpReadData\n"
+    ".type sceHttpReadData @function\n"
+    "sceHttpReadData:\n");
+
+asm(".global sceHttpRedirectCacheFlush\n"
+    ".type sceHttpRedirectCacheFlush @function\n"
+    "sceHttpRedirectCacheFlush:\n");
+
+asm(".global sceHttpRemoveRequestHeader\n"
+    ".type sceHttpRemoveRequestHeader @function\n"
+    "sceHttpRemoveRequestHeader:\n");
+
+asm(".global sceHttpRequestGetAllHeaders\n"
+    ".type sceHttpRequestGetAllHeaders @function\n"
+    "sceHttpRequestGetAllHeaders:\n");
+
+asm(".global sceHttpSendRequest\n"
+    ".type sceHttpSendRequest @function\n"
+    "sceHttpSendRequest:\n");
+
+asm(".global sceHttpSetAcceptEncodingGZIPEnabled\n"
+    ".type sceHttpSetAcceptEncodingGZIPEnabled @function\n"
+    "sceHttpSetAcceptEncodingGZIPEnabled:\n");
+
+asm(".global sceHttpSetAuthEnabled\n"
+    ".type sceHttpSetAuthEnabled @function\n"
+    "sceHttpSetAuthEnabled:\n");
+
+asm(".global sceHttpSetAuthInfoCallback\n"
+    ".type sceHttpSetAuthInfoCallback @function\n"
+    "sceHttpSetAuthInfoCallback:\n");
+
+asm(".global sceHttpSetAutoRedirect\n"
+    ".type sceHttpSetAutoRedirect @function\n"
+    "sceHttpSetAutoRedirect:\n");
+
+asm(".global sceHttpSetChunkedTransferEnabled\n"
+    ".type sceHttpSetChunkedTransferEnabled @function\n"
+    "sceHttpSetChunkedTransferEnabled:\n");
+
+asm(".global sceHttpSetConnectTimeOut\n"
+    ".type sceHttpSetConnectTimeOut @function\n"
+    "sceHttpSetConnectTimeOut:\n");
+
+asm(".global sceHttpSetCookieEnabled\n"
+    ".type sceHttpSetCookieEnabled @function\n"
+    "sceHttpSetCookieEnabled:\n");
+
+asm(".global sceHttpSetCookieMaxNum\n"
+    ".type sceHttpSetCookieMaxNum @function\n"
+    "sceHttpSetCookieMaxNum:\n");
+
+asm(".global sceHttpSetCookieMaxNumPerDomain\n"
+    ".type sceHttpSetCookieMaxNumPerDomain @function\n"
+    "sceHttpSetCookieMaxNumPerDomain:\n");
+
+asm(".global sceHttpSetCookieMaxSize\n"
+    ".type sceHttpSetCookieMaxSize @function\n"
+    "sceHttpSetCookieMaxSize:\n");
+
+asm(".global sceHttpSetCookieRecvCallback\n"
+    ".type sceHttpSetCookieRecvCallback @function\n"
+    "sceHttpSetCookieRecvCallback:\n");
+
+asm(".global sceHttpSetCookieSendCallback\n"
+    ".type sceHttpSetCookieSendCallback @function\n"
+    "sceHttpSetCookieSendCallback:\n");
+
+asm(".global sceHttpSetCookieTotalMaxSize\n"
+    ".type sceHttpSetCookieTotalMaxSize @function\n"
+    "sceHttpSetCookieTotalMaxSize:\n");
+
+asm(".global sceHttpSetDefaultAcceptEncodingGZIPEnabled\n"
+    ".type sceHttpSetDefaultAcceptEncodingGZIPEnabled @function\n"
+    "sceHttpSetDefaultAcceptEncodingGZIPEnabled:\n");
+
+asm(".global sceHttpSetDelayBuildRequestEnabled\n"
+    ".type sceHttpSetDelayBuildRequestEnabled @function\n"
+    "sceHttpSetDelayBuildRequestEnabled:\n");
+
+asm(".global sceHttpSetEpoll\n"
+    ".type sceHttpSetEpoll @function\n"
+    "sceHttpSetEpoll:\n");
+
+asm(".global sceHttpSetEpollId\n"
+    ".type sceHttpSetEpollId @function\n"
+    "sceHttpSetEpollId:\n");
+
+asm(".global sceHttpSetHttp09Enabled\n"
+    ".type sceHttpSetHttp09Enabled @function\n"
+    "sceHttpSetHttp09Enabled:\n");
+
+asm(".global sceHttpSetInflateGZIPEnabled\n"
+    ".type sceHttpSetInflateGZIPEnabled @function\n"
+    "sceHttpSetInflateGZIPEnabled:\n");
+
+asm(".global sceHttpSetNonblock\n"
+    ".type sceHttpSetNonblock @function\n"
+    "sceHttpSetNonblock:\n");
+
+asm(".global sceHttpSetPolicyOption\n"
+    ".type sceHttpSetPolicyOption @function\n"
+    "sceHttpSetPolicyOption:\n");
+
+asm(".global sceHttpSetPriorityOption\n"
+    ".type sceHttpSetPriorityOption @function\n"
+    "sceHttpSetPriorityOption:\n");
+
+asm(".global sceHttpSetProxy\n"
+    ".type sceHttpSetProxy @function\n"
+    "sceHttpSetProxy:\n");
+
+asm(".global sceHttpSetRecvBlockSize\n"
+    ".type sceHttpSetRecvBlockSize @function\n"
+    "sceHttpSetRecvBlockSize:\n");
+
+asm(".global sceHttpSetRecvTimeOut\n"
+    ".type sceHttpSetRecvTimeOut @function\n"
+    "sceHttpSetRecvTimeOut:\n");
+
+asm(".global sceHttpSetRedirectCallback\n"
+    ".type sceHttpSetRedirectCallback @function\n"
+    "sceHttpSetRedirectCallback:\n");
+
+asm(".global sceHttpSetRequestContentLength\n"
+    ".type sceHttpSetRequestContentLength @function\n"
+    "sceHttpSetRequestContentLength:\n");
+
+asm(".global sceHttpSetRequestStatusCallback\n"
+    ".type sceHttpSetRequestStatusCallback @function\n"
+    "sceHttpSetRequestStatusCallback:\n");
+
+asm(".global sceHttpSetResolveRetry\n"
+    ".type sceHttpSetResolveRetry @function\n"
+    "sceHttpSetResolveRetry:\n");
+
+asm(".global sceHttpSetResolveTimeOut\n"
+    ".type sceHttpSetResolveTimeOut @function\n"
+    "sceHttpSetResolveTimeOut:\n");
+
+asm(".global sceHttpSetResponseHeaderMaxSize\n"
+    ".type sceHttpSetResponseHeaderMaxSize @function\n"
+    "sceHttpSetResponseHeaderMaxSize:\n");
+
+asm(".global sceHttpSetSendTimeOut\n"
+    ".type sceHttpSetSendTimeOut @function\n"
+    "sceHttpSetSendTimeOut:\n");
+
+asm(".global sceHttpSetSocketCreationCallback\n"
+    ".type sceHttpSetSocketCreationCallback @function\n"
+    "sceHttpSetSocketCreationCallback:\n");
+
+asm(".global sceHttpTerm\n"
+    ".type sceHttpTerm @function\n"
+    "sceHttpTerm:\n");
+
+asm(".global sceHttpTryGetNonblock\n"
+    ".type sceHttpTryGetNonblock @function\n"
+    "sceHttpTryGetNonblock:\n");
+
+asm(".global sceHttpTrySetNonblock\n"
+    ".type sceHttpTrySetNonblock @function\n"
+    "sceHttpTrySetNonblock:\n");
+
+asm(".global sceHttpUnsetEpoll\n"
+    ".type sceHttpUnsetEpoll @function\n"
+    "sceHttpUnsetEpoll:\n");
+
+asm(".global sceHttpUriBuild\n"
+    ".type sceHttpUriBuild @function\n"
+    "sceHttpUriBuild:\n");
+
+asm(".global sceHttpUriCopy\n"
+    ".type sceHttpUriCopy @function\n"
+    "sceHttpUriCopy:\n");
+
+asm(".global sceHttpUriEscape\n"
+    ".type sceHttpUriEscape @function\n"
+    "sceHttpUriEscape:\n");
+
+asm(".global sceHttpUriMerge\n"
+    ".type sceHttpUriMerge @function\n"
+    "sceHttpUriMerge:\n");
+
+asm(".global sceHttpUriParse\n"
+    ".type sceHttpUriParse @function\n"
+    "sceHttpUriParse:\n");
+
+asm(".global sceHttpUriSweepPath\n"
+    ".type sceHttpUriSweepPath @function\n"
+    "sceHttpUriSweepPath:\n");
+
+asm(".global sceHttpUriUnescape\n"
+    ".type sceHttpUriUnescape @function\n"
+    "sceHttpUriUnescape:\n");
+
+asm(".global sceHttpWaitRequest\n"
+    ".type sceHttpWaitRequest @function\n"
+    "sceHttpWaitRequest:\n");
+
+asm(".global sceHttpsDisableOption\n"
+    ".type sceHttpsDisableOption @function\n"
+    "sceHttpsDisableOption:\n");
+
+asm(".global sceHttpsDisableOptionPrivate\n"
+    ".type sceHttpsDisableOptionPrivate @function\n"
+    "sceHttpsDisableOptionPrivate:\n");
+
+asm(".global sceHttpsEnableOption\n"
+    ".type sceHttpsEnableOption @function\n"
+    "sceHttpsEnableOption:\n");
+
+asm(".global sceHttpsEnableOptionPrivate\n"
+    ".type sceHttpsEnableOptionPrivate @function\n"
+    "sceHttpsEnableOptionPrivate:\n");
+
+asm(".global sceHttpsFreeCaList\n"
+    ".type sceHttpsFreeCaList @function\n"
+    "sceHttpsFreeCaList:\n");
+
+asm(".global sceHttpsGetCaList\n"
+    ".type sceHttpsGetCaList @function\n"
+    "sceHttpsGetCaList:\n");
+
+asm(".global sceHttpsGetSslError\n"
+    ".type sceHttpsGetSslError @function\n"
+    "sceHttpsGetSslError:\n");
+
+asm(".global sceHttpsLoadCert\n"
+    ".type sceHttpsLoadCert @function\n"
+    "sceHttpsLoadCert:\n");
+
+asm(".global sceHttpsSetMinSslVersion\n"
+    ".type sceHttpsSetMinSslVersion @function\n"
+    "sceHttpsSetMinSslVersion:\n");
+
+asm(".global sceHttpsSetSslCallback\n"
+    ".type sceHttpsSetSslCallback @function\n"
+    "sceHttpsSetSslCallback:\n");
+
+asm(".global sceHttpsSetSslVersion\n"
+    ".type sceHttpsSetSslVersion @function\n"
+    "sceHttpsSetSslVersion:\n");
+
+asm(".global sceHttpsUnloadCert\n"
+    ".type sceHttpsUnloadCert @function\n"
+    "sceHttpsUnloadCert:\n");
